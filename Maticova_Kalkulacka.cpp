63ba0452b38e2e85f0d93aba67b014c3ef523d7d
Lokit58 <vitecek73@gmail.com>
2023-10-14 17:07:45 +0200

    Přidejte soubory projektu.

---
+++ Maticova_Kalkulacka.cpp
@@ -0,0 +1,12 @@
+﻿// Maticova_Kalkulacka.cpp: Definuje vstupní bod pro aplikaci.
+//
+
+#include "Maticova_Kalkulacka.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
