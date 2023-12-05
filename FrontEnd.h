a861151a64d75506e598b8b84eb8705a5d2d3e41
Lokit58 <vitecek73@gmail.com>
2023-12-05 19:49:54 +0100

    Added comments

--- FrontEnd.h
+++ FrontEnd.h
@@ -3,31 +3,43 @@
 #include<Windows.h>
 #include<conio.h>
 #include"MatrixCalc.h"
-
+//Funkce na vytisknuti matice bez kurzoru
 void printRes(Matrix m);
 
+//Funkce která èeká na libovolnı input od uivatele
 void waitTillPressed();
 
+//Funkce která zobrazí návod
 void tutorial();
 
+//Funkce na vıpis menu
 void printMainMenu(short selectedRow, const char* options[], int sizeOfOptions);
 
+//Funkce která zobrazí úvodní obrazovku
 void startScreen();
 
+//Funkce která spouští a provozuje hlavní menu
 int mainMenu(const char* options[], int sizeOfOptions);
 
+//Funkce na naplnìní matice nulama
 void fillMatrix(Matrix* m);
 
+//Funkce na vytisknuti matice s kurzorem
 void printMatrix(Matrix* m, int x, int y);
 
+//Funcke která naskenuje èíslo od uivatele
 double scanNum();
 
+//Funkce která uèuje kam se bude zapisovat do matice
 int matrixSelection(Matrix* m);
 
+//Funkce která zapisuje do matice
 int matrixInput(Matrix* m);
 
+//Funkce která vypisuje a provozuje meny kalkulaèky
 int calculationMenu(Matrix* matA, Matrix* matB, const char* options[], int sizeOfOptions);
 
+//Funkce která zajišuje pøedání matice k vıpoètùm
 int calculationHandle(Matrix* matA, Matrix* matB, const char* options[], int sizeOfOptions, Matrix* result);
 
 //------------------------------------------------------------------ HISTORY ------------------------------------------------------
--- MatrixCalc.h
+++ MatrixCalc.h
@@ -39,19 +39,26 @@ Matrix multiplyWithScalar(Matrix A, int Scalar);
 
 // Funkce pro výpoèet determinantu
 
-
+//Analýza ve kterou øádku nebo sloupci matice je nejvíce 0
 int analizeMat(Matrix mat);
 
+//Zmenšení matice 4x4 na 3x3 podle nejlepšího øádku/sloupce
 Matrix createMat3x3Shrink(Matrix mat, int col, int row);
 
+//Pomocná funcke pro poèítání determinantu 4x4 - podle øádkù
 double calculateDet4x4Row(Matrix mat, int bestRow);
 
+//Pomocná funcke pro poèítání determinantu 4x4 - podle slpupcù
 double calculateDet4x4Col(Matrix mat, int bestCol);
 
+//Funkce pro poèítání determinantu 2x2
 double calculateDet2x2(Matrix mat);
 
+//Funkce pro poèítání determinantu 3x3
 double calculateDet3x3(Matrix mat);
 
+//Funkce pro poèítání determinantu 4x4
 double calculateDet4x4(Matrix mat);
 
+//Funcke pro poèítání obecného determinantu
 double calculateDeterminant(Matrix mat, int* err);
